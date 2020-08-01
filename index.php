4aca7222201c579124382e4d91c18795002003e3
Jimd3v <jeremy.gth.dev@gmail.com>
2020-08-01 16:08:29 +0200

    Add content

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jérémy Gauthier</title>
+    <meta name="description" content="Bienvenu sur le CV en ligne de Jérémy Gauthier, vous y trouverez toutes les infos concernant mon parcours et mes expériences"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/aos.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles/main.css" rel="stylesheet">
+  </head>
+  <body id="top">
+    <header>
+      <div class="profile-page sidebar-collapse">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-transparent bg-primary" color-on-scroll="400">
+          <div class="container">
+            <div class="navbar-translate"><a class="navbar-brand" href="#" rel="tooltip">Mon CV</a>
+              <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-bar bar1"></span><span class="navbar-toggler-bar bar2"></span><span class="navbar-toggler-bar bar3"></span></button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation">
+              <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#about">A propos</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#skill">Compétences</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#portfolio">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#experience">Experience</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <div class="page-content">
+      <div>
+<div class="profile-page">
+  <div class="wrapper">
+    <div class="page-header page-header-small" filter-color="green">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('images/cc-bg-1.jpg');"></div>
+      <div class="container">
+        <div class="content-center">
+          <div class="cc-profile-image"><a href="#"><img src="images/jim.jpg" alt="photo de profil"/></a></div>
+          <div class="h2 title">Jérémy Gauthier</div>
+          <p class="category text-white">Web Developpeur</p><a class="btn btn-primary smooth-scroll mr-2" href="#contact" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Me contacter</a><a class="btn btn-primary" href="images/CV-jeremy.pdf" data-aos="zoom-in" data-aos-anchor="data-aos-anchor"download>Télécharger</a>
+        </div>
+      </div>
+      <div class="section">
+        <div class="container">
+          <div class="button-container"><a class="btn btn-default btn-round btn-lg btn-icon" href="https://www.linkedin.com/in/jeremygauthier11/" rel="tooltip" title="Suivez moi sur linkedin"><i class="fa fa-linkedin"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="https://twitter.com/jimd3v" rel="tooltip" title="Suivez moi sur Twitter"><i class="fa fa-twitter"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="https://github.com/jeremy-gauthier" rel="tooltip" title="Suivez moi sur Github"><i class="fa fa-github"></i></a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section" id="about">
+  <div class="container">
+    <div class="card" data-aos="fade-up" data-aos-offset="10">
+      <div class="row">
+        <div class="col-lg-6 col-md-12">
+          <div class="card-body">
+            <div class="h4 mt-0 title">Hello World</div>
+            <p>Bonjour, je m'appelle Jérémy Gauthier et je suis developpeur web junior. Je suis actuellement à l'écoute du marché</p>
+            <p>Vous retrouverez ici toutes les informations concernant mes compétences, ainsi que mes différentes expériences</p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12">
+          <div class="card-body">
+            <div class="h4 mt-0 title">Informations</div>
+            <div class="row">
+              <div class="col-sm-4"><strong class="text-uppercase">Age:</strong></div>
+              <div class="col-sm-8">28</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Email:</strong></div>
+              <div class="col-sm-8">jeremy.gth.dev@gmail.com</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Mobile:</strong></div>
+              <div class="col-sm-8">06 52 63 92 00</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Localisation:</strong></div>
+              <div class="col-sm-8">Entre Toulouse et Carcassonne</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section" id="skill">
+  <div class="container">
+    <div class="h4 text-center mb-4 title">Mes compétences</div>
+    <div class="card" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">HTML - CSS (Bootstrap, Bulma)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div><span class="progress-value">80%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">PHP (Symfony, Lumen)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div><span class="progress-value">75%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">JavaScript (VueJS en cours d'apprentissage)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div><span class="progress-value">60%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">API REST</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div><span class="progress-value">80%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">GIT</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div><span class="progress-value">75%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">Wordpress</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div><span class="progress-value">80%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">Base de donnée (MYSQL)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div><span class="progress-value">80%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">OS (Microsoft,Linux)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="75" style="width: 70%;"></div><span class="progress-value">75%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">Python (Django)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"></div><span class="progress-value">50%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">System Admin (déploiement sur serveur AWS)</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="75" style="width: 70%;"></div><span class="progress-value">70%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+
+
+
+    </div>
+  </div>
+</div>
+<div class="section" id="experience">
+  <div class="container cc-experience">
+    <div class="h4 text-center mb-4 title">Mon Experience</div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-experience-header">
+            <p>Juillet 2020</p>
+            <div class="h5">La main au panier</div>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <div class="h5">Developpeur Back End Symfony</div>
+            <p>Developpement d'une plateforme de mise en relation entre producteurs et consommateurs souhaitant privilégier les circuits courts et les produits locaux. <br><br>Le site est codé en Symfony coté Back, et en React coté Front.
+            <br><br> <a href="https://www.youtube.com/watch?v=sSp3eACAeU0&t=1941s">Lien vers la présentation du projet</a> (à partir de 7"30)
+            <br><br>
+            <a href="http://3.94.211.25/">Lien vers la plateforme</a>
+            </p>
+            <br>
+          <img src="images/lamainaupanier.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-experience-header">
+            <p>Janvier 2020</p>
+            <div class="h5">Site vitrine Wordpress</div>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <div class="h5">Elodie Ferrier - Aide à domicile</div>
+            <p>Création du site vitrine <a href="http://elodie-aad.fr">Elodie-AAD</a> pour une entreprise de prestation de service sur Castelnaudary</p>
+            <img src="images/elodie.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-experience-header">
+            <p>2020</p>
+            <div class="h5">Projet Personnel</div>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <div class="h5">Mediatheque en Symfony</div>
+            <p>Proje personnel de médiateque codé avec Symfony<br><br>
+            <a href="https://github.com/jeremy-gauthier/Mediatek">Lien Github</a></p>
+            <img src="images/media.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section">
+  <div class="container cc-education">
+    <div class="h4 text-center mb-4 title">Formation</div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-education-header">
+          <p>Fevrier 2020 - Juillet 2020</p>
+            <div class="h5">Titre Professionnel</div>
+            <div class="h5">Développeur Web et Web Mobile</div>
+            <p>O'clock</p>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+         <p>La formation “Développeur web” prépare au titre professionnel “Développeur Web et Web Mobile” (Bac +2). Toute la formation se déroule en téléprésentiel sur l'environnement Linux avec utilisation quotidienne de terminal et GIT.<br>
+          <br>
+          Socle Développement Web (448 heures sur 13 semaines) :
+          HTML, CSS ( Bootstrap, Bulma ), JavaScript, PHP ( Lumen, Symfony ), mySQL , architecture MVC, méthode agile, hébergement web.<br><br>
+Spécialisation sur Symfony (140 heures sur 4 semaines)<br><br>
+Réalisation d’un projet (4 semaines)
+<br><br><a href="https://oclock.io/formations/developpeur-web">Lien vers la formation</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-education-header">
+          <p>2012 - 2014</p>
+            <div class="h5">Bachelor</div>
+            <div class="h5">Marketing Vente</div>
+            <p class="category">ISTEF Toulouse</p>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+          <br>
+         <p>Bachelor Marketing vente (Bac +3) obtenu en 2016 à l'école de commerce ISTEF Toulouse <br><br> <a href="https://www.istef.fr/SVE/Uploads/Fichiers/BACHELOR-MKV.pdf">Lien vers la formation</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-education-header">
+            <p>2012 - 2014</p>
+            <div class="h5">BTS</div>
+            <div class="h5">Management des Unitées Commerciales</div>
+            <p class="category">IDRAC Montpellier</p>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <br>
+            <p>BTS Management des unités commerciales (Bac +2) obtenu en 2014 à l'école de commerce IDRAC Montpellier <br><br><a href="https://www.cned.fr/inscription/22961dix?gclid=Cj0KCQjwgo_5BRDuARIsADDEntR_3R4R_SkL_Iiaotg56BXqg8HAs_knO9iHx6nz863MrII9senIrsQaAujlEALw_wcB&gclsrc=aw.ds">Lien vers la formation</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section" id="reference">
+  <div class="container cc-reference">
+  </div>
+</div>
+<div class="section" id="contact">
+  <div class="cc-contact-information" style="background-image: url('images/staticmap.png');">
+    <div class="container">
+      <div class="cc-contact">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="card mb-0" data-aos="zoom-in">
+              <div class="h4 text-center title">Me Contacter</div>
+              <div class="row">
+                
+                <div class="col-md-12">
+                  <div class="card-body text-center">
+                    <!-- <p class="mb-0"><strong>Address</strong></p>
+                    <p class="pb-2">140, City Center, New York, U.S.A</p> -->
+                    <p class="mb-0"><strong>Mobile</strong></p>
+                    <p>06 52 63 92 00</p>
+                    <p class="mb-0"><strong>Email</strong></p>
+                    <p>jeremy.gth.dev@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div></div>
+    </div>
+    </body>
+    <footer class="footer">
+      <div class="container text-center"><a class="cc-github btn btn-link" href="https://github.com/jeremy-gauthier"><i class="fa fa-github fa-2x " aria-hidden="true"></i></a><a class="cc-twitter btn btn-link " href="https://twitter.com/jimd3v"><i class="fa fa-twitter fa-2x " aria-hidden="true"></i></a><a class="cc-linkedin btn btn-link" href="https://www.linkedin.com/in/jeremygauthier11/"><i class="fa fa-linkedin fa-2x " aria-hidden="true"></i></a></div>
+      <div class="h4 title text-center">Jérémy Gauthier</div>
+      <div class="text-center text-muted">
+        <p>&copy; Jérémy Gauthier. All rights reserved</p>
+      </div>
+    </footer>
+    <script src="js/core/jquery.3.2.1.min.js"></script>
+    <script src="js/core/popper.min.js"></script>
+    <script src="js/core/bootstrap.min.js"></script>
+    <script src="js/now-ui-kit.js?v=1.1.0"></script>
+    <script src="js/aos.js"></script>
+    <script src="scripts/main.js"></script>
+  </body>
+</html>
---
+++ project1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> projet 1</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="wrapper">
+
+    <div class="photo-1"><img src="https://c0.lestechnophiles.com/www.numerama.com/content/uploads/2020/01/apple_shot-on-iphone-challenge-2020_austin-mann_01072020_big-jpg-large.jpg?resize=1212,712" alt=""> </div>
+    <div class="photo-2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcROCmdRsbs8OkyJK5O1SSiFfRIlqWxyJC8bjVh-7lofPu97_UQi&usqp=CAU" alt=""> </div>
+    <div class="photo-3"><img src="https://lamanzanamordida.net/app/uploads-lamanzanamordida.net/2018/11/2351F1C2-EDC0-4CEA-8675-5AE5924B63E1.jpeg" alt=""> </div>
+    <div class="photo-4"><img src="https://www.faq-mac.com/wp-content/uploads/2019/02/Retrato.jpg" alt=""> </div>
+
+<h1 class="titre">Lorem</h1>
+<p class="descript">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam iure officia maxime temporibus dolor. Quos architecto porro, atque similique necessitatibus soluta aliquid corrupti excepturi saepe laboriosam, id, odit alias sunt.</p>
+</div>
+</body>
+</html>
